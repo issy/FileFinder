2f20832037c7eef1d4c05e85367d4e22e0229906
Issy Szemeti <issy.szemeti@starlingbank.com>
2024-11-19 08:47:58 +0000

    Remove TODO message

--- src/main/java/com/issy/compiler/TokenContext.java
+++ src/main/java/com/issy/compiler/TokenContext.java
@@ -1,6 +1,5 @@
 package com.issy.compiler;
 
-// TODO: Include line:col position data for better error messages
 public record TokenContext(Token token, String value, Integer line, Integer column) {
 
   public static TokenContext withPosition(Token token, String value, Lexer.Position position) {
